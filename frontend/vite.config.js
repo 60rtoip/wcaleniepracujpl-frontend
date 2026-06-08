edd64c79cc717958af2ce5b7a008bcf25e288c05
PiotrW <160155342+60rtoip@users.noreply.github.com>
2026-06-09 01:00:19 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    host: true, // żeby działało w Dockerze
+    host: true,
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://api:8000', // ✅ było localhost
+        target: 'http://api:8000',
         changeOrigin: true,
         secure: false,
       },
       '/minio': {
-        target: 'http://minio:9000', // ✅ było localhost
+        target: 'http://minio:9000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/minio/, ''),
